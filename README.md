0aa761240e949c95557c1ba3365fc3e00e5afedd
Hathem Tarbah <hathemtarbah@Wizards-Air.lan>
2025-05-24 23:28:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Cursor Lock
+
+A Python script that locks the mouse cursor to the primary monitor.  
+Designed for users who want to restrict cursor movement.
+
+## Features
+
+- Locks cursor within the main monitor's boundaries.
+- Lightweight, runs in the background.
+- Python-powered solution.
+
+## Requirements
+
+- `pyautogui`
+- `screeninfo`
+
+Install dependencies:
+```bash
+pip install pyautogui screeninfo
---
+++ cursor_lock.py
@@ -0,0 +1,28 @@
+from screeninfo import get_monitors
+import pyautogui
+import time
+
+monitor = get_monitors()[0] #because monitor 0 is (usually) primary
+
+left = monitor.x
+top = monitor.y
+right = left + monitor.width
+bottom = top + monitor.height
+
+print(f"monitor bounds: left={left}; top={top}; right={right}; bottom={bottom}")
+
+while True:
+
+    x, y = pyautogui.position()
+
+    if x < left:
+        x = left
+    elif x > right - 1:
+        x = right -1
+    if y < top:
+        y = top
+    elif y > bottom - 1:
+        y = bottom - 1
+
+    pyautogui.moveTo(x, y)
+    time.sleep(0.01) #to lower cpu usage
